5bfa1e58574b0fdb5c6a61a6509a37b8f0c662fb
Lisa Saffel <58794458+lsaffel@users.noreply.github.com>
2023-07-03 11:38:24 -0600

    added some comments to main.py

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ def removeDuplicatesFromLinkedList(linkedList):
     b = a.next
 
     while b != None:
+        # advance both pointers until the values are different
         while b != None and a.value == b.value:
             b = b.next
 
+        # check if the end of the list has been found or not
         if b != None:
             if a.value == b.value:
                 a.next = None
@@ -19,6 +21,10 @@ def removeDuplicatesFromLinkedList(linkedList):
                 a.next = b
                 a = b
                 b = b.next
+
+        # b is pointing past the end of the list, so the values are the same. 
+        # Therefore, the last node is a duplicate, so point a at None, 
+        # to remove the last duplicate node.
         else:
             a.next = None
     return linkedList
